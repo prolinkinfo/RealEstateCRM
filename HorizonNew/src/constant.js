d37401e8f64c3a4c37efe7896bd15ee5d8c1fd39
Denish-prolink <denish.prolink@gmail.com>
2023-08-16 10:05:37 +0530

    change  base_url from front-end

--- HorizonNew/src/constant.js
+++ HorizonNew/src/constant.js
@@ -1,5 +1,5 @@
 export const constant = {
-    baseUrl: 'https://outsales-backed.onrender.com/'
+    baseUrl: 'https://reale-state-crm.onrender.com'
     // baseUrl: process.env.REACT_APP_BASE_URL
     // baseUrl: 'http://127.0.0.1:5001/'
 }
