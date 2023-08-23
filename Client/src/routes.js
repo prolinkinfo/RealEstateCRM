cea3a802fc8ca5e02f6ba782f29a151c7079efef
Denish-prolink <denish.prolink@gmail.com>
2023-08-23 11:39:54 +0530

    change Dashboard name

--- Client/src/routes.js
+++ Client/src/routes.js
@@ -60,7 +60,7 @@ const SignInCentered = React.lazy(() => import("views/auth/signIn"));
 const routes = [
   // ========================== Dashboard ==========================
   {
-    name: "Main Dashboard",
+    name: "Dashboard",
     layout: "/admin",
     path: "/default",
     icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
