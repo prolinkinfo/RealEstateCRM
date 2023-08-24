66231e011023d582089806eacaaf404bfb32608c
Denish-prolink <denish.prolink@gmail.com>
2023-08-24 14:56:11 +0530

    On login page - after entering password if we press enter then also it should login

--- Client/src/views/auth/signIn/index.jsx
+++ Client/src/views/auth/signIn/index.jsx
@@ -111,149 +111,84 @@ function SignIn() {
           mx={{ base: "auto", lg: "unset" }}
           me='auto'
           mb={{ base: "20px", md: "auto" }}>
-          {/* <Button
-            fontSize='sm'
-            me='0px'
-            mb='26px'
-            py='15px'
-            h='50px'
-            borderRadius='16px'
-            bg={googleBg}
-            color={googleText}
-            fontWeight='500'
-            _hover={googleHover}
-            _active={googleActive}
-            _focus={googleActive}>
-            <Icon as={FcGoogle} w='20px' h='20px' me='10px' />
-            Sign in with Google
-          </Button>
-          <Flex align='center' mb='25px'>
-            <HSeparator />
-            <Text color='gray.400' mx='14px'>
-              or
-            </Text>
-            <HSeparator />
-          </Flex> */}
-          <FormControl isInvalid={errors.username && touched.username} >
-            <FormLabel
-              display='flex'
-              ms='4px'
-              fontSize='sm'
-              fontWeight='500'
-              color={textColor}
-              mb='8px'>
-              Email<Text color={brandStars}>*</Text>
-            </FormLabel>
-            <Input
-              // isRequired={true}
-              // variant='auth'
-              fontSize='sm'
-              onChange={handleChange} onBlur={handleBlur}
-              value={values.username}
-              name="username"
-              ms={{ base: "0px", md: "0px" }}
-              type='email'
-              placeholder='mail@simmmple.com'
-              mb={errors.username && touched.username ? undefined : '24px'}
-              fontWeight='500'
-              size='lg'
-              borderColor={errors.username && touched.username ? "red.300" : null}
-              className={errors.username && touched.username ? "isInvalid" : null}
-            />
-            {errors.username && touched.username && <FormErrorMessage mb='24px'> {errors.username}</FormErrorMessage>}
-          </FormControl>
-
-          <FormControl isInvalid={errors.password && touched.password} mb="24px">
-            <FormLabel
-              ms='4px'
-              fontSize='sm'
-              fontWeight='500'
-              color={textColor}
-              display='flex'>
-              Password<Text color={brandStars}>*</Text>
-            </FormLabel>
-            <InputGroup size='md'>
-              <Input
-                isRequired={true}
+          <form onSubmit={handleSubmit}>
+            <FormControl isInvalid={errors.username && touched.username} >
+              <FormLabel
+                display='flex'
+                ms='4px'
                 fontSize='sm'
-                placeholder='Enter Your Password'
-                name='password'
-                mb={errors.password && touched.password ? undefined : '24px'}
-                value={values.password} onChange={handleChange} onBlur={handleBlur}
+                fontWeight='500'
+                color={textColor}
+                mb='8px'>
+                Email<Text color={brandStars}>*</Text>
+              </FormLabel>
+              <Input
+                fontSize='sm'
+                onChange={handleChange} onBlur={handleBlur}
+                value={values.username}
+                name="username"
+                ms={{ base: "0px", md: "0px" }}
+                type='email'
+                placeholder='mail@simmmple.com'
+                mb={errors.username && touched.username ? undefined : '24px'}
+                fontWeight='500'
                 size='lg'
-                variant='auth'
-                type={show ? "text" : "password"}
-                borderColor={errors.password && touched.password ? "red.300" : null}
-                className={errors.password && touched.password ? "isInvalid" : null}
+                borderColor={errors.username && touched.username ? "red.300" : null}
+                className={errors.username && touched.username ? "isInvalid" : null}
               />
-              <InputRightElement display='flex' alignItems='center' mt='4px'>
-                <Icon
-                  color={textColorSecondary}
-                  _hover={{ cursor: "pointer" }}
-                  as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
-                  onClick={showPass}
-                />
-              </InputRightElement>
-            </InputGroup>
-            {errors.password && touched.password && <FormErrorMessage mb='24px'> {errors.password}</FormErrorMessage>}
-            <Flex justifyContent='space-between' align='center' mb='24px'>
-              {/* <FormControl display='flex' alignItems='center'>
-                <Checkbox
-                  id='remember-login'
-                  colorScheme='brandScheme'
-                  me='10px'
-                />
-                <FormLabel
-                  htmlFor='remember-login'
-                  mb='0'
-                  fontWeight='normal'
-                  color={textColor}
-                  fontSize='sm'>
-                  Keep me logged in
-                </FormLabel>
-              </FormControl> */}
-              {/* <NavLink to='/auth/forgot-password'>
-                <Text
-                  color={textColorBrand}
+              {errors.username && touched.username && <FormErrorMessage mb='24px'> {errors.username}</FormErrorMessage>}
+            </FormControl>
+
+            <FormControl isInvalid={errors.password && touched.password} mb="24px">
+              <FormLabel
+                ms='4px'
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                display='flex'>
+                Password<Text color={brandStars}>*</Text>
+              </FormLabel>
+              <InputGroup size='md'>
+                <Input
+                  isRequired={true}
                   fontSize='sm'
-                  w='124px'
-                  fontWeight='500'>
-                  Forgot password?
-                </Text>
-              </NavLink> */}
-            </Flex>
-            <Button
-              fontSize='sm'
-              variant='brand'
-              fontWeight='500'
-              w='100%'
-              h='50'
-              onClick={handleSubmit}
-              mb='24px'
-              disabled={isLoding ? true : false}
-            >
-              {isLoding ? <Spinner /> : 'Sign In'}
-            </Button>
-          </FormControl>
-          {/* <Flex
-            flexDirection='column'
-            justifyContent='center'
-            alignItems='start'
-            maxW='100%'
-            mt='0px'>
-            <Text color={textColorDetails} fontWeight='400' fontSize='14px'>
-              Not registered yet?
-              <NavLink to='/auth/sign-up'>
-                <Text
-                  color={textColorBrand}
-                  as='span'
-                  ms='5px'
-                  fontWeight='500'>
-                  Create an Account
-                </Text>
-              </NavLink>
-            </Text>
-          </Flex> */}
+                  placeholder='Enter Your Password'
+                  name='password'
+                  mb={errors.password && touched.password ? undefined : '24px'}
+                  value={values.password} onChange={handleChange} onBlur={handleBlur}
+                  size='lg'
+                  variant='auth'
+                  type={show ? "text" : "password"}
+                  borderColor={errors.password && touched.password ? "red.300" : null}
+                  className={errors.password && touched.password ? "isInvalid" : null}
+                />
+                <InputRightElement display='flex' alignItems='center' mt='4px'>
+                  <Icon
+                    color={textColorSecondary}
+                    _hover={{ cursor: "pointer" }}
+                    as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
+                    onClick={showPass}
+                  />
+                </InputRightElement>
+              </InputGroup>
+              {errors.password && touched.password && <FormErrorMessage mb='24px'> {errors.password}</FormErrorMessage>}
+              <Flex justifyContent='space-between' align='center' mb='24px'>
+              </Flex>
+              <Button
+                fontSize='sm'
+                variant='brand'
+                fontWeight='500'
+                w='100%'
+                h='50'
+                // onClick={handleSubmit}
+                type="submit"
+                mb='24px'
+                disabled={isLoding ? true : false}
+              >
+                {isLoding ? <Spinner /> : 'Sign In'}
+              </Button>
+            </FormControl>
+          </form>
         </Flex>
       </Flex>
     </DefaultAuth>
