92364cc1e39049ddea75a2a51c44804b7b2e39b0
Denish-prolink <denish.prolink@gmail.com>
2023-08-25 16:06:44 +0530

    email view api

--- server/controllers/emailHistory/email.js
+++ server/controllers/emailHistory/email.js
@@ -116,22 +116,73 @@ const view = async (req, res) => {
 
         if (!result) return res.status(404).json({ message: "no Data Found." })
 
+        // let response = await EmailHistory.aggregate([
+        //     { $match: { _id: result._id } },
+        //     {
+        //         $lookup: {
+        //             from: 'contacts', // Assuming this is the collection name for 'contacts'
+        //             localField: 'createBy',
+        //             foreignField: '_id',
+        //             as: 'contact'
+        //         }
+        //     },
+        //     {
+        //         $lookup: {
+        //             from: 'leads', // Assuming this is the collection name for 'leads'
+        //             localField: 'createByLead',
+        //             foreignField: '_id',
+        //             as: 'createByrefLead'
+        //         }
+        //     },
+        //     {
+        //         $lookup: {
+        //             from: 'users',
+        //             localField: 'sender',
+        //             foreignField: '_id',
+        //             as: 'users'
+        //         }
+        //     },
+        //     { $unwind: { path: '$users', preserveNullAndEmptyArrays: true } },
+        //     { $unwind: '$contact' },
+        //     { $match: { 'users.deleted': false } },
+        //     {
+        //         $addFields: {
+        //             senderEmail: '$users.username',
+
+        //             deleted: {
+        //                 $cond: [
+        //                     { $eq: ['$contact.deleted', false] },
+        //                     '$contact.deleted',
+        //                     { $ifNull: ['$createByrefLead.deleted', false] }
+        //                 ]
+        //             },
+        //             createByName: {
+        //                 $cond: {
+        //                     if: '$contact',
+        //                     then: { $concat: ['$contact.title', ' ', '$contact.firstName', ' ', '$contact.lastName'] },
+        //                     else: { $concat: ['$createByrefLead.leadName'] }
+        //                 }
+        //             },
+        //         }
+        //     },
+        //     { $project: { contact: 0, users: 0 } }
+        // ])
         let response = await EmailHistory.aggregate([
             { $match: { _id: result._id } },
-            {
-                $lookup: {
-                    from: 'contacts',
-                    localField: 'createBy',
-                    foreignField: '_id',
-                    as: 'contact'
-                }
-            },
             {
                 $lookup: {
                     from: 'leads', // Assuming this is the collection name for 'leads'
                     localField: 'createByLead',
                     foreignField: '_id',
                     as: 'createByrefLead'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'contacts', // Assuming this is the collection name for 'contacts'
+                    localField: 'createBy',
+                    foreignField: '_id',
+                    as: 'createByRef'
                 }
             },
             {
@@ -143,32 +194,38 @@ const view = async (req, res) => {
                 }
             },
             { $unwind: { path: '$users', preserveNullAndEmptyArrays: true } },
-            { $unwind: '$contact' },
+            { $unwind: { path: '$createByRef', preserveNullAndEmptyArrays: true } },
+            { $unwind: { path: '$createByrefLead', preserveNullAndEmptyArrays: true } },
             { $match: { 'users.deleted': false } },
             {
                 $addFields: {
                     senderEmail: '$users.username',
-
                     deleted: {
                         $cond: [
-                            { $eq: ['$contact.deleted', false] },
-                            '$contact.deleted',
+                            { $eq: ['$createByRef.deleted', false] },
+                            '$createByRef.deleted',
                             { $ifNull: ['$createByrefLead.deleted', false] }
                         ]
                     },
                     createByName: {
                         $cond: {
-                            if: '$contact',
-                            then: { $concat: ['$contact.title', ' ', '$contact.firstName', ' ', '$contact.lastName'] },
+                            if: '$createByRef',
+                            then: { $concat: ['$createByRef.title', ' ', '$createByRef.firstName', ' ', '$createByRef.lastName'] },
                             else: { $concat: ['$createByrefLead.leadName'] }
                         }
                     },
                 }
             },
-            { $project: { contact: 0, createByrefLead: 0, users: 0 } }
+            {
+                $project: {
+                    createByRef: 0,
+                    createByrefLead: 0,
+                    users: 0
+                }
+            },
         ])
 
-        res.status(200).json(response[0])
+        res.status(200).json(response)
     } catch (err) {
         console.error('Failed :', err);
         res.status(400).json({ err, error: 'Failed ' });
