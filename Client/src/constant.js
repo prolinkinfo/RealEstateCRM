52f565d2c34fff8a83b0087d8d49c8dacbb07325
Denish-prolink <denish.prolink@gmail.com>
2023-08-25 11:07:21 +0530

    change url

--- Client/src/constant.js
+++ Client/src/constant.js
@@ -1,4 +1,4 @@
 export const constant = {
-    // baseUrl: 'https://reale-state-crm.onrender.com/'
-    baseUrl: 'http://127.0.0.1:5001/'
+    baseUrl: 'https://reale-state-crm.onrender.com/'
+    // baseUrl: 'http://127.0.0.1:5001/'
 }
