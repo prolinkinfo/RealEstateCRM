8234a912d65e0af35017921ca0fb529126fffe80
Denish-prolink <denish.prolink@gmail.com>
2023-09-01 11:33:28 +0530

    .env file for frontend

--- Client/src/constant.js
+++ Client/src/constant.js
@@ -1,4 +1,5 @@
 export const constant = {
-    baseUrl: 'https://reale-state-crm.onrender.com/'
+    baseUrl: process.env.REACT_APP_BASE_URL
+    // baseUrl: 'https://reale-state-crm.onrender.com/'
     // baseUrl: 'http://127.0.0.1:5001/'
 }
