272e6bba3d373db7d3981591fcfafec9454170e7
Denish-prolink <prafullsatasia@gmail.com>
2023-09-05 11:30:18 +0530

    change log out mesage

--- Client/src/components/navbar/NavbarLinksAdmin.js
+++ Client/src/components/navbar/NavbarLinksAdmin.js
@@ -1,77 +1,80 @@
 // Chakra Imports
 import {
-	Avatar, Flex,
-	Icon, Menu,
+	Avatar,
+	Flex,
+	Icon,
+	Menu,
 	MenuButton,
 	MenuItem,
 	MenuList,
 	Text,
-	useColorModeValue
-} from '@chakra-ui/react';
+	useColorModeValue,
+} from "@chakra-ui/react";
 // Custom Components
-import { ItemContent } from 'components/menu/ItemContent';
-import { SearchBar } from 'components/navbar/searchBar/SearchBar';
-import { SidebarResponsive } from 'components/sidebar/Sidebar';
-import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
+import { ItemContent } from "components/menu/ItemContent";
+import { SearchBar } from "components/navbar/searchBar/SearchBar";
+import { SidebarResponsive } from "components/sidebar/Sidebar";
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 // Assets
-import { MdNotificationsNone } from 'react-icons/md';
-import { FaEthereum } from 'react-icons/fa';
-import routes from 'routes.js';
-import { useNavigate } from 'react-router-dom';
-import { getApi } from 'services/api';
-import { toast } from 'react-toastify';
-import jwtDecode from 'jwt-decode';
+import { MdNotificationsNone } from "react-icons/md";
+import { FaEthereum } from "react-icons/fa";
+import routes from "routes.js";
+import { useNavigate } from "react-router-dom";
+import { getApi } from "services/api";
+import { toast } from "react-toastify";
+import jwtDecode from "jwt-decode";
 
 export default function HeaderLinks(props) {
 	const { secondary } = props;
 	// Chakra Color Mode
-	const navbarIcon = useColorModeValue('gray.400', 'white');
-	let menuBg = useColorModeValue('white', 'navy.800');
-	const textColor = useColorModeValue('secondaryGray.900', 'white');
-	const textColorBrand = useColorModeValue('brand.700', 'brand.400');
-	const ethColor = useColorModeValue('gray.700', 'white');
-	const borderColor = useColorModeValue('#E6ECFA', 'rgba(135, 140, 189, 0.3)');
-	const ethBg = useColorModeValue('secondaryGray.300', 'navy.900');
-	const ethBox = useColorModeValue('white', 'navy.800');
+	const navbarIcon = useColorModeValue("gray.400", "white");
+	let menuBg = useColorModeValue("white", "navy.800");
+	const textColor = useColorModeValue("secondaryGray.900", "white");
+	const textColorBrand = useColorModeValue("brand.700", "brand.400");
+	const ethColor = useColorModeValue("gray.700", "white");
+	const borderColor = useColorModeValue("#E6ECFA", "rgba(135, 140, 189, 0.3)");
+	const ethBg = useColorModeValue("secondaryGray.300", "navy.900");
+	const ethBox = useColorModeValue("white", "navy.800");
 	const shadow = useColorModeValue(
-		'14px 17px 40px 4px rgba(112, 144, 176, 0.18)',
-		'14px 17px 40px 4px rgba(112, 144, 176, 0.06)'
+		"14px 17px 40px 4px rgba(112, 144, 176, 0.18)",
+		"14px 17px 40px 4px rgba(112, 144, 176, 0.06)"
 	);
 	// const borderButton = useColorModeValue('secondaryGray.500', 'whiteAlpha.200');
 
-	const [loginUser, setLoginUser] = useState()
-	const navigate = useNavigate()
+	const [loginUser, setLoginUser] = useState();
+	const navigate = useNavigate();
 
-	const user = loginUser?.firstName + ' ' + loginUser?.lastName;
+	const user = loginUser?.firstName + " " + loginUser?.lastName;
 
-	const userId = JSON.parse(localStorage.getItem('user'))?._id;
+	const userId = JSON.parse(localStorage.getItem("user"))?._id;
 
 	const fetchData = async () => {
-		let response = await getApi('api/user/view/', userId)
+		let response = await getApi("api/user/view/", userId);
 		setLoginUser(response.data);
-	}
+	};
 
 	useEffect(() => {
-		fetchData()
-	}, [])
+		fetchData();
+	}, []);
 
 	const [isLogoutScheduled, setIsLogoutScheduled] = useState(false);
 
 	const logOut = (message) => {
 		localStorage.clear();
 		sessionStorage.clear();
-		navigate('/auth');
+		navigate("/auth");
 		if (message) {
 			toast.error(message);
 		} else {
-			toast.success('Logged out automatically due to token expiration');
+			toast.success("Log out Successfully");
 		}
 		setIsLogoutScheduled(true);
 	};
 
 	useEffect(() => {
-		const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+		const token =
+			localStorage.getItem("token") || sessionStorage.getItem("token");
 
 		if (token) {
 			try {
@@ -79,52 +82,70 @@ export default function HeaderLinks(props) {
 				const currentTime = Date.now() / 1000; // Convert milliseconds to seconds
 				if (decodedToken.exp < currentTime) {
 					if (!isLogoutScheduled) {
-						logOut('Token has expired');
+						logOut("Token has expired");
 					}
 				} else {
 					// Schedule automatic logout when the token expires
 					const timeToExpire = (decodedToken.exp - currentTime) * 1000; // Convert seconds to milliseconds
 					setTimeout(() => {
 						if (!isLogoutScheduled) {
-							logOut('Token has expired');
+							logOut("Token has expired");
 						}
 					}, timeToExpire);
 				}
 			} catch (error) {
-				console.error('Error decoding token:', error);
+				console.error("Error decoding token:", error);
 			}
 		}
 	}, [isLogoutScheduled]);
 
-
-
 	return (
 		<Flex
-			w={{ sm: '100%', md: 'auto' }}
+			w={{ sm: "100%", md: "auto" }}
 			alignItems="center"
 			flexDirection="row"
 			bg={menuBg}
-			flexWrap={secondary ? { base: 'wrap', md: 'nowrap' } : 'unset'}
+			flexWrap={secondary ? { base: "wrap", md: "nowrap" } : "unset"}
 			p="10px"
 			borderRadius="30px"
-			boxShadow={shadow}>
-			<SearchBar mb={secondary ? { base: '10px', md: 'unset' } : 'unset'} me="10px" borderRadius="30px" />
+			boxShadow={shadow}
+		>
+			<SearchBar
+				mb={secondary ? { base: "10px", md: "unset" } : "unset"}
+				me="10px"
+				borderRadius="30px"
+			/>
 
 			<Flex
 				bg={ethBg}
-				display={secondary ? 'flex' : 'none'}
+				display={secondary ? "flex" : "none"}
 				borderRadius="30px"
 				ms="auto"
 				p="6px"
 				align="center"
-				me="6px">
-				<Flex align="center" justify="center" bg={ethBox} h="29px" w="29px" borderRadius="30px" me="7px">
+				me="6px"
+			>
+				<Flex
+					align="center"
+					justify="center"
+					bg={ethBox}
+					h="29px"
+					w="29px"
+					borderRadius="30px"
+					me="7px"
+				>
 					<Icon color={ethColor} w="9px" h="14px" as={FaEthereum} />
 				</Flex>
-				<Text w="max-content" color={ethColor} fontSize="sm" fontWeight="700" me="6px">
+				<Text
+					w="max-content"
+					color={ethColor}
+					fontSize="sm"
+					fontWeight="700"
+					me="6px"
+				>
 					1,924
-					<Text as="span" display={{ base: 'none', md: 'unset' }}>
-						{' '}
+					<Text as="span" display={{ base: "none", md: "unset" }}>
+						{" "}
 						ETH
 					</Text>
 				</Text>
@@ -132,9 +153,16 @@ export default function HeaderLinks(props) {
 
 			<SidebarResponsive routes={routes} />
 
-			<Menu >
+			<Menu>
 				<MenuButton p="0px">
-					<Icon mt="6px" as={MdNotificationsNone} color={navbarIcon} w="18px" h="18px" me="10px" />
+					<Icon
+						mt="6px"
+						as={MdNotificationsNone}
+						color={navbarIcon}
+						w="18px"
+						h="18px"
+						me="10px"
+					/>
 				</MenuButton>
 				<MenuList
 					boxShadow={shadow}
@@ -143,23 +171,45 @@ export default function HeaderLinks(props) {
 					bg={menuBg}
 					border="none"
 					mt="22px"
-					me={{ base: '30px', md: 'unset' }}
-					minW={{ base: 'unset', md: '400px', xl: '450px' }}
-					maxW={{ base: '360px', md: 'unset' }}>
+					me={{ base: "30px", md: "unset" }}
+					minW={{ base: "unset", md: "400px", xl: "450px" }}
+					maxW={{ base: "360px", md: "unset" }}
+				>
 					<Flex jusitfy="space-between" w="100%" mb="20px">
 						<Text fontSize="md" fontWeight="600" color={textColor}>
 							Notifications
 						</Text>
-						<Text fontSize="sm" fontWeight="500" color={textColorBrand} ms="auto" cursor="pointer">
+						<Text
+							fontSize="sm"
+							fontWeight="500"
+							color={textColorBrand}
+							ms="auto"
+							cursor="pointer"
+						>
 							Mark all read
 						</Text>
 					</Flex>
 					<Flex flexDirection="column">
-						<MenuItem _hover={{ bg: 'none' }} _focus={{ bg: 'none' }} px="0" borderRadius="8px" mb="10px">
+						<MenuItem
+							_hover={{ bg: "none" }}
+							_focus={{ bg: "none" }}
+							px="0"
+							borderRadius="8px"
+							mb="10px"
+						>
 							<ItemContent info="Horizon UI Dashboard PRO" aName="Alicia" />
 						</MenuItem>
-						<MenuItem _hover={{ bg: 'none' }} _focus={{ bg: 'none' }} px="0" borderRadius="8px" mb="10px">
-							<ItemContent info="Horizon Design System Free" aName="Josh Henry" />
+						<MenuItem
+							_hover={{ bg: "none" }}
+							_focus={{ bg: "none" }}
+							px="0"
+							borderRadius="8px"
+							mb="10px"
+						>
+							<ItemContent
+								info="Horizon Design System Free"
+								aName="Josh Henry"
+							/>
 						</MenuItem>
 					</Flex>
 				</MenuList>
@@ -229,16 +279,23 @@ export default function HeaderLinks(props) {
 			<Menu style={{ zIndex: 1500 }}>
 				<MenuButton p="0px">
 					<Avatar
-						_hover={{ cursor: 'pointer' }}
+						_hover={{ cursor: "pointer" }}
 						color="white"
-						name={user || 'Prolink Infotech'}
+						name={user || "Prolink Infotech"}
 						bg="#11047A"
 						size="sm"
 						w="40px"
 						h="40px"
 					/>
 				</MenuButton>
-				<MenuList boxShadow={shadow} p="0px" mt="10px" borderRadius="20px" bg={menuBg} border="none">
+				<MenuList
+					boxShadow={shadow}
+					p="0px"
+					mt="10px"
+					borderRadius="20px"
+					bg={menuBg}
+					border="none"
+				>
 					<Flex w="100%" mb="0px">
 						<Text
 							ps="20px"
@@ -249,29 +306,56 @@ export default function HeaderLinks(props) {
 							borderColor={borderColor}
 							fontSize="sm"
 							fontWeight="700"
-							textTransform={'capitalize'}
-							color={textColor}>
+							textTransform={"capitalize"}
+							color={textColor}
+						>
 							👋&nbsp; Hey, {user}
 						</Text>
 					</Flex>
 
 					<Flex flexDirection="column" p="10px">
-						<MenuItem _hover={{ bg: 'none' }} _focus={{ bg: 'none' }} borderRadius="8px" px="14px">
-							<Text fontSize="sm" onClick={() => navigate(`/admin/`)} >Home</Text>
+						<MenuItem
+							_hover={{ bg: "none" }}
+							_focus={{ bg: "none" }}
+							borderRadius="8px"
+							px="14px"
+						>
+							<Text fontSize="sm" onClick={() => navigate(`/admin/`)}>
+								Home
+							</Text>
 						</MenuItem>
-						<MenuItem _hover={{ bg: 'none' }} _focus={{ bg: 'none' }} borderRadius="8px" px="14px">
-							<Text fontSize="sm" onClick={() => navigate(JSON.parse(localStorage.getItem('user'))?.role === 'admin' ? `/admin/userView/${JSON.parse(localStorage.getItem('user'))?._id}` : `/userView/${JSON.parse(localStorage.getItem('user'))?._id}`)} >Profile Settings</Text>
+						<MenuItem
+							_hover={{ bg: "none" }}
+							_focus={{ bg: "none" }}
+							borderRadius="8px"
+							px="14px"
+						>
+							<Text
+								fontSize="sm"
+								onClick={() =>
+									navigate(
+										JSON.parse(localStorage.getItem("user"))?.role === "admin"
+											? `/admin/userView/${JSON.parse(localStorage.getItem("user"))?._id
+											}`
+											: `/userView/${JSON.parse(localStorage.getItem("user"))?._id
+											}`
+									)
+								}
+							>
+								Profile Settings
+							</Text>
 						</MenuItem>
 						{/*<MenuItem _hover={{ bg: 'none' }} _focus={{ bg: 'none' }} borderRadius="8px" px="14px">
 							<Text fontSize="sm">Newsletter Settings</Text>
 						</MenuItem> */}
 						<MenuItem
-							_hover={{ bg: 'none' }}
+							_hover={{ bg: "none" }}
 							onClick={logOut}
-							_focus={{ bg: 'none' }}
+							_focus={{ bg: "none" }}
 							color="red.400"
 							borderRadius="8px"
-							px="14px">
+							px="14px"
+						>
 							<Text fontSize="sm">Log out</Text>
 						</MenuItem>
 					</Flex>
@@ -285,5 +369,5 @@ HeaderLinks.propTypes = {
 	variant: PropTypes.string,
 	fixed: PropTypes.bool,
 	secondary: PropTypes.bool,
-	onOpen: PropTypes.func
+	onOpen: PropTypes.func,
 };
