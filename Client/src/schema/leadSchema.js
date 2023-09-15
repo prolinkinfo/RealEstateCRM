6d490b48597249ec53864a5a1d3c8243292e40d6
Denish-prolink <prafullsatasia@gmail.com>
2023-09-15 11:49:03 +0530

    fix phone Number validation

--- Client/src/schema/leadSchema.js
+++ Client/src/schema/leadSchema.js
@@ -4,7 +4,7 @@ export const leadSchema = yup.object({
     // Lead Information:
     leadName: yup.string().required("Lead Name Is required"),
     leadEmail: yup.string().email().required("Lead Email Is required"),
-    leadPhoneNumber: yup.string().min(999999999, 'Phone number is invalid').max(999999999999, 'Phone number is invalid').required("Lead Phone Number Is required"),
+    leadPhoneNumber: yup.string().min(1000000000, 'Phone number is invalid').max(999999999999, 'Phone number is invalid').required("Lead Phone Number Is required"),
     leadAddress: yup.string().required("Lead Address Is required"),
     // Lead Source and Details:
     leadSource: yup.string(),
