4522b8eb3c58fc6d4e6722341900043fbd7e1bae
Denish-prolink <prafullsatasia@gmail.com>
2023-10-03 15:39:00 +0530

    set phone validation on User model

--- Client/src/schema/userSchema.js
+++ Client/src/schema/userSchema.js
@@ -3,6 +3,6 @@ import * as yup from 'yup'
 export const userSchema = yup.object({
     firstName: yup.string().required("First Name Is required"),
     lastName: yup.string(),
-    phoneNumber: yup.string().required("Phone Number Is required"),
+    phoneNumber: yup.string().required("Phone Number Is required").matches(/^\d{10}$/, "Phone Number must be exactly 10 digits"),
     username: yup.string().email().required("Email Is required"),
 })
