42156bdc840307b89a78db28a8d39ced36d1e8ae
Denish-prolink <prafullsatasia@gmail.com>
2023-10-05 12:15:03 +0530

    ``` This commit removes the yup.object schema for emails.

--- Client/src/schema/emailSchema.js
+++ Client/src/schema/emailSchema.js
@@ -19,17 +19,3 @@ export const emailSchema = yup.object({
         });
     }
 });
-
-
-// export const emailSchema = yup.object({
-//     sender: yup.string().required("Sender Is required"),
-//     recipient: yup.string().email().required("Recipient Is required"),
-//     cc: yup.string().email(),
-//     bcc: yup.string().email(),
-//     relatedToContact: yup.string(),
-//     relatedToLead: yup.string(),
-//     subject: yup.string(),
-//     message: yup.string(),
-//     createBy: yup.string(),
-//     createByLead: yup.string()
-// })
