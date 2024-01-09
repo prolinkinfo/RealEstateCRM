5e7cec0b1dd41616524134d846ed00275663b84e
Happy <happy.specscale@gmail.com>
2024-01-09 16:08:22 +0530

    'property_module_add_permission'

--- Client/src/views/admin/property/components/CheckTable.js
+++ Client/src/views/admin/property/components/CheckTable.js
@@ -313,7 +313,7 @@ export default function CheckTable(props) {
               </InputGroup>
               <Button variant="outline" colorScheme='brand' leftIcon={<SearchIcon />} onClick={() => setAdvaceSearch(true)} size="sm">Advance Search</Button>
               {displaySearchData === true ? <Button variant="outline" size="sm" colorScheme='red' ms={2} onClick={() => { handleClear(); setSearchbox(''); setGetTagValues([]) }}>clear</Button> : ""}
-              {selectedValues.length > 0 && <DeleteIcon onClick={() => setDelete(true)} color={'red'} ms={2} />}
+              {(selectedValues.length > 0 && access?.delete) && <DeleteIcon onClick={() => setDelete(true)} color={'red'} ms={2} />}
             </Flex>
           </GridItem>
           <GridItem colSpan={4} display={"flex"} justifyContent={"end"} alignItems={"center"} textAlign={"right"}>
