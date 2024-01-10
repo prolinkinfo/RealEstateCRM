e226c78bce502a8e32453a615aea2264fe960d66
Rutubhimani28 <rutubhimani.specscale@gmail.com>
2024-01-10 16:45:35 +0530

    push code

--- Client/src/components/sidebar/components/Brand.js
+++ Client/src/components/sidebar/components/Brand.js
@@ -1,4 +1,3 @@
-import React from "react";
 
 // Chakra imports
 import { Flex, Heading, useColorModeValue } from "@chakra-ui/react";
@@ -7,16 +6,22 @@ import { Flex, Heading, useColorModeValue } from "@chakra-ui/react";
 import { HSeparator } from "components/separator/Separator";
 
 export function SidebarBrand(props) {
-  const { setOpenSidebar, openSidebar } = props;
+  const { setOpenSidebar, openSidebar, from } = props;
 
   //   Chakra color mode
   let logoColor = useColorModeValue("navy.700", "white");
 
   return (
     <Flex align='center' direction='column'>
-      <Heading my={4} onClick={() => setOpenSidebar(!openSidebar)} cursor={"pointer"} userSelect={"none"}>{openSidebar === true ? "Prolink" : "Pr"}</Heading>
+      <Flex>
+        <Heading my={4}
+          cursor={"pointer"} onClick={() => !from && setOpenSidebar(!openSidebar)} userSelect={"none"}>{openSidebar === true ? "Prolink" : "Pr"}</Heading>
+        {/* <Text border={"1px solid gray"} borderRadius={"50%"} my={4} onClick={() => setOpenSidebar(!openSidebar)} cursor={"pointer"} userSelect={"none"}
+          display={{ sm: "none", xl: "inline-flex" }} fontSize={"22px"}
+        >{openSidebar === true ? <HiOutlineChevronLeft /> : <HiOutlineChevronRight />}</Text> */}
+
+      </Flex>
       <HSeparator
-      // mb='20px'
       />
     </Flex>
   );
