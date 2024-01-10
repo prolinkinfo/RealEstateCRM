1dacf20be93737d7affd1f7b6c480a6596ef9e2e
Rutubhimani28 <rutubhimani.specscale@gmail.com>
2024-01-10 17:23:26 +0530

    Update SidebarCard component styles

--- Client/src/components/sidebar/components/SidebarCard.js
+++ Client/src/components/sidebar/components/SidebarCard.js
@@ -12,7 +12,6 @@ import React from "react";
 export default function SidebarDocs() {
   const bgColor = "linear-gradient(135deg, #868CFF 0%, #4318FF 100%)";
   const borderColor = useColorModeValue("white", "navy.800");
-
   return (
     <Flex
       justify='center'
