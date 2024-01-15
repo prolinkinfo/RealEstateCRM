1a287640632fd58419c0db6b3842da2dae70de18
Happy <happy.specscale@gmail.com>
2024-01-15 17:19:27 +0530

    sidebar dynamic logo

--- Client/src/components/sidebar/components/Brand.js
+++ Client/src/components/sidebar/components/Brand.js
@@ -1,15 +1,27 @@
 
 // Chakra imports
-import { Flex, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Heading, Image, useColorModeValue } from "@chakra-ui/react";
 
 // Custom components
 import { HSeparator } from "components/separator/Separator";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchImage } from "../../../redux/imageSlice";
 
 export function SidebarBrand(props) {
   const { setOpenSidebar, openSidebar, from } = props;
 
   //   Chakra color mode
   let logoColor = useColorModeValue("navy.700", "white");
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Dispatch the fetchRoles action on component mount
+    dispatch(fetchImage("?isActive=true"));
+  }, [dispatch]);
+
+  const image = useSelector((state) => state?.images?.image);
 
   return (
     <Flex align='center' direction='column' style={{
@@ -19,8 +31,17 @@ export function SidebarBrand(props) {
       background: "#fff"
     }}>
       <Flex>
-        <Heading my={4}
-          cursor={"pointer"} onClick={() => !from && setOpenSidebar(!openSidebar)} userSelect={"none"}>{openSidebar === true ? "Prolink" : "Pr"}</Heading>
+        {/* <Heading my={4}
+          cursor={"pointer"} onClick={() => !from && setOpenSidebar(!openSidebar)} userSelect={"none"}>{openSidebar === true ? "Prolink" : "Pr"}</Heading> */}
+        <Image
+          style={{ width: "150px", height: '60px' }}
+          src={openSidebar === true ? image[0]?.logoLgImg : image[0]?.logoSmImg} // Set the source path of your image
+          alt="Logo" // Set the alt text for accessibility
+          cursor="pointer"
+          onClick={() => !from && setOpenSidebar(!openSidebar)}
+          userSelect="none"
+          my={2}
+        />
         {/* <Text border={"1px solid gray"} borderRadius={"50%"} my={4} onClick={() => setOpenSidebar(!openSidebar)} cursor={"pointer"} userSelect={"none"}
           display={{ sm: "none", xl: "inline-flex" }} fontSize={"22px"}
         >{openSidebar === true ? <HiOutlineChevronLeft /> : <HiOutlineChevronRight />}</Text> */}
