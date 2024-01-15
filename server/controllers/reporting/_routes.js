9a05d60834510c00ba8efdba3f9c4ef303ae4643
Denish-prolink <prafullsatasia@gmail.com>
2024-01-15 12:42:55 +0530

    Add line chart route and handler to reporting module

--- server/controllers/reporting/_routes.js
+++ server/controllers/reporting/_routes.js
@@ -5,6 +5,7 @@ const auth = require('../../middelwares/auth');
 const router = express.Router();
 
 router.get('/', auth, reporting.index)
+router.get('/line-chart', auth, reporting.lineChart)
 router.post('/index', auth, reporting.data)
 
 
