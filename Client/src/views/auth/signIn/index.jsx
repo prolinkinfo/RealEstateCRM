e1aec1170dd47f0553e51b80ce4f244c91a0310c
Happy <happy.specscale@gmail.com>
2024-01-15 16:44:58 +0530

    dynamic-image-redux

--- Client/src/views/auth/signIn/index.jsx
+++ Client/src/views/auth/signIn/index.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 // Chakra imports
 import {
   Box,
-  Button, Checkbox, Flex,
+  Button,
+  Checkbox,
+  Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
@@ -14,9 +16,8 @@ import {
   InputGroup,
   InputRightElement,
   Text,
-  useColorModeValue
+  useColorModeValue,
 } from "@chakra-ui/react";
-
 
 // Custom components
 import DefaultAuth from "layouts/auth/Default";
@@ -29,140 +30,184 @@ import { postApi } from "services/api";
 import { loginSchema } from "schema";
 import { toast } from "react-toastify";
 import Spinner from "components/spinner/Spinner";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchImage } from "../../../redux/imageSlice";
 
 function SignIn() {
   // Chakra color mode
   const textColor = useColorModeValue("navy.700", "white");
   const textColorSecondary = "gray.400";
   const brandStars = useColorModeValue("brand.500", "brand.400");
-  const [isLoding, setIsLoding] = React.useState(false)
-  const [checkBox, setCheckBox] = React.useState(true)
+  const [isLoding, setIsLoding] = React.useState(false);
+  const [checkBox, setCheckBox] = React.useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Dispatch the fetchRoles action on component mount
+    dispatch(fetchImage("?isActive=true"));
+  }, [dispatch]);
+
+  const image = useSelector((state) => state?.images?.image);
 
   const [show, setShow] = React.useState(false);
   const showPass = () => setShow(!show);
 
   const initialValues = {
-    username: '',
-    password: ''
-  }
-  const { errors, values, touched, handleBlur, handleChange, resetForm, handleSubmit } = useFormik({
+    username: "",
+    password: "",
+  };
+  const {
+    errors,
+    values,
+    touched,
+    handleBlur,
+    handleChange,
+    resetForm,
+    handleSubmit,
+  } = useFormik({
     initialValues: initialValues,
     validationSchema: loginSchema,
     onSubmit: (values, { resetForm }) => {
-      login()
-    }
-  })
-  const navigate = useNavigate()
+      login();
+    },
+  });
+  const navigate = useNavigate();
 
   const login = async () => {
     try {
-      setIsLoding(true)
-      let response = await postApi('api/user/login', values, checkBox)
+      setIsLoding(true);
+      let response = await postApi("api/user/login", values, checkBox);
       if (response && response.status === 200) {
-        navigate('/superAdmin')
-        toast.success("Login Successfully!")
+        navigate("/superAdmin");
+        toast.success("Login Successfully!");
         resetForm();
       } else {
-        toast.error(response.response.data?.error)
+        toast.error(response.response.data?.error);
       }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoding(false);
     }
-    catch (e) {
-      console.log(e)
-    }
-    finally {
-      setIsLoding(false)
-    }
-  }
+  };
 
   return (
-    <DefaultAuth illustrationBackground={illustration} image={illustration}>
+    <DefaultAuth
+      illustrationBackground={image[0]?.authImg}
+      image={image[0]?.authImg}
+    >
       <Flex
         maxW={{ base: "100%", md: "max-content" }}
-        w='100%'
+        w="100%"
         mx={{ base: "auto", lg: "0px" }}
-        me='auto'
-        h='fit-content'
-        alignItems='start'
-        justifyContent='center'
+        me="auto"
+        h="fit-content"
+        alignItems="start"
+        justifyContent="center"
         mb={{ base: "30px", md: "60px" }}
         px={{ base: "25px", md: "0px" }}
         mt={{ base: "40px", md: "14vh" }}
-        flexDirection='column'>
-        <Box me='auto'>
-          <Heading color={textColor} fontSize='36px' mb='10px'>
+        flexDirection="column"
+      >
+        <Box me="auto">
+          <Heading color={textColor} fontSize="36px" mb="10px">
             Sign In
           </Heading>
           <Text
-            mb='36px'
-            ms='4px'
+            mb="36px"
+            ms="4px"
             color={textColorSecondary}
-            fontWeight='400'
-            fontSize='md'>
+            fontWeight="400"
+            fontSize="md"
+          >
             Enter your email and password to sign in!
           </Text>
         </Box>
         <Flex
-          zIndex='2'
-          direction='column'
+          zIndex="2"
+          direction="column"
           w={{ base: "100%", md: "420px" }}
-          maxW='100%'
-          background='transparent'
-          borderRadius='15px'
+          maxW="100%"
+          background="transparent"
+          borderRadius="15px"
           mx={{ base: "auto", lg: "unset" }}
-          me='auto'
-          mb={{ base: "20px", md: "auto" }}>
+          me="auto"
+          mb={{ base: "20px", md: "auto" }}
+        >
           <form onSubmit={handleSubmit}>
-            <FormControl isInvalid={errors.username && touched.username} >
+            <FormControl isInvalid={errors.username && touched.username}>
               <FormLabel
-                display='flex'
-                ms='4px'
-                fontSize='sm'
-                fontWeight='500'
+                display="flex"
+                ms="4px"
+                fontSize="sm"
+                fontWeight="500"
                 color={textColor}
-                mb='8px'>
+                mb="8px"
+              >
                 Email<Text color={brandStars}>*</Text>
               </FormLabel>
               <Input
-                fontSize='sm'
-                onChange={handleChange} onBlur={handleBlur}
+                fontSize="sm"
+                onChange={handleChange}
+                onBlur={handleBlur}
                 value={values.username}
-                name="username" 
+                name="username"
                 ms={{ base: "0px", md: "0px" }}
-                type='email'
-                placeholder='mail@simmmple.com'
-                mb={errors.username && touched.username ? undefined : '24px'}
-                fontWeight='500'
-                size='lg'
-                borderColor={errors.username && touched.username ? "red.300" : null}
-                className={errors.username && touched.username ? "isInvalid" : null}
+                type="email"
+                placeholder="mail@simmmple.com"
+                mb={errors.username && touched.username ? undefined : "24px"}
+                fontWeight="500"
+                size="lg"
+                borderColor={
+                  errors.username && touched.username ? "red.300" : null
+                }
+                className={
+                  errors.username && touched.username ? "isInvalid" : null
+                }
               />
-              {errors.username && touched.username && <FormErrorMessage mb='24px'> {errors.username}</FormErrorMessage>}
+              {errors.username && touched.username && (
+                <FormErrorMessage mb="24px">
+                  {" "}
+                  {errors.username}
+                </FormErrorMessage>
+              )}
             </FormControl>
 
-            <FormControl isInvalid={errors.password && touched.password} mb="24px">
+            <FormControl
+              isInvalid={errors.password && touched.password}
+              mb="24px"
+            >
               <FormLabel
-                ms='4px'
-                fontSize='sm'
-                fontWeight='500'
+                ms="4px"
+                fontSize="sm"
+                fontWeight="500"
                 color={textColor}
-                display='flex'>
+                display="flex"
+              >
                 Password<Text color={brandStars}>*</Text>
               </FormLabel>
-              <InputGroup size='md'>
+              <InputGroup size="md">
                 <Input
                   isRequired={true}
-                  fontSize='sm'
-                  placeholder='Enter Your Password'
-                  name='password'
-                  mb={errors.password && touched.password ? undefined : '24px'}
-                  value={values.password} onChange={handleChange} onBlur={handleBlur}
-                  size='lg'
-                  variant='auth'
+                  fontSize="sm"
+                  placeholder="Enter Your Password"
+                  name="password"
+                  mb={errors.password && touched.password ? undefined : "24px"}
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  size="lg"
+                  variant="auth"
                   type={show ? "text" : "password"}
-                  borderColor={errors.password && touched.password ? "red.300" : null}
-                  className={errors.password && touched.password ? "isInvalid" : null}
+                  borderColor={
+                    errors.password && touched.password ? "red.300" : null
+                  }
+                  className={
+                    errors.password && touched.password ? "isInvalid" : null
+                  }
                 />
-                <InputRightElement display='flex' alignItems='center' mt='4px'>
+                <InputRightElement display="flex" alignItems="center" mt="4px">
                   <Icon
                     color={textColorSecondary}
                     _hover={{ cursor: "pointer" }}
@@ -171,41 +216,50 @@ function SignIn() {
                   />
                 </InputRightElement>
               </InputGroup>
-              {errors.password && touched.password && <FormErrorMessage mb='24px'> {errors.password}</FormErrorMessage>}
-              <Flex justifyContent='space-between' align='center' mb='24px'>
-                <FormControl display='flex' alignItems='center'>
+              {errors.password && touched.password && (
+                <FormErrorMessage mb="24px">
+                  {" "}
+                  {errors.password}
+                </FormErrorMessage>
+              )}
+              <Flex justifyContent="space-between" align="center" mb="24px">
+                <FormControl display="flex" alignItems="center">
                   <Checkbox
                     onChange={(e) => setCheckBox(e.target.checked)}
-                    id='remember-login'
+                    id="remember-login"
                     value={checkBox}
                     defaultChecked
-                    colorScheme='brandScheme'
-                    me='10px'
+                    colorScheme="brandScheme"
+                    me="10px"
                   />
                   <FormLabel
-                    htmlFor='remember-login'
-                    mb='0'
-                    fontWeight='normal'
+                    htmlFor="remember-login"
+                    mb="0"
+                    fontWeight="normal"
                     color={textColor}
-                    fontSize='sm'>
+                    fontSize="sm"
+                  >
                     Keep me logged in
                   </FormLabel>
                 </FormControl>
               </Flex>
 
-              <Flex justifyContent='space-between' align='center' mb='24px'>
-              </Flex>
+              <Flex
+                justifyContent="space-between"
+                align="center"
+                mb="24px"
+              ></Flex>
               <Button
-                fontSize='sm'
-                variant='brand'
-                fontWeight='500'
-                w='100%'
-                h='50'
+                fontSize="sm"
+                variant="brand"
+                fontWeight="500"
+                w="100%"
+                h="50"
                 type="submit"
-                mb='24px'
+                mb="24px"
                 disabled={isLoding ? true : false}
               >
-                {isLoding ? <Spinner /> : 'Sign In'}
+                {isLoding ? <Spinner /> : "Sign In"}
               </Button>
             </FormControl>
           </form>
