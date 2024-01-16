fdad4b016a8d4884f14ffaebe53433e792fd67de
Happy <happy.specscale@gmail.com>
2024-01-16 13:02:00 +0530

    remove unused console

--- Client/src/redux/imageSlice.js
+++ Client/src/redux/imageSlice.js
@@ -12,7 +12,6 @@ const initialState = {
 export const fetchImage = createAsyncThunk('roles/fetchRoles', async (active) => {
     try {
         const response = await getApi(`api/images/${active ? active : ""}`);
-        console.log(response.data)
         return response.data;
     } catch (error) {
         throw error;
