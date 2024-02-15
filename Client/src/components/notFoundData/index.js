43ab01877dc98a02f62a7867c37066ed42ec6a1e
Happy <happy.specscale@gmail.com>
2024-02-15 18:03:03 +0530

    remove unuse commient

--- Client/src/components/notFoundData/index.js
+++ Client/src/components/notFoundData/index.js
@@ -1,11 +1,9 @@
 import { Image, Box } from '@chakra-ui/react'
-// import dataImage from '../../assets/img/nodatafound1.png'
 import React from 'react'
 
 const DataNotFound = () => {
     return (
         <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
-            {/* <Image src={dataImage} /> */}
             -- No Data Found --
         </Box>
     )
