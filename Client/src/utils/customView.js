14c66c06697c4fb67020e80d1028303003b0ace5
Happy <happy.specscale@gmail.com>
2024-02-19 16:50:32 +0530

    md srceen responsive

--- Client/src/utils/customView.js
+++ Client/src/utils/customView.js
@@ -7,7 +7,9 @@ import { HSeparator } from "components/separator/Separator";
 const CustomView = ({ data, toCamelCase, fieldData }) => {
 
     const headingLength = data?.headings?.length % 3
+    const headingLengthMd = data?.headings?.length % 2
     const lastLength = data?.headings.length - headingLength
+    const lastLengthMd = data?.headings.length - headingLengthMd
 
     // Define a function to determine the colSpan value
     const getColSpanLg = (ind, lastLength, headingLength) => {
@@ -17,13 +19,20 @@ const CustomView = ({ data, toCamelCase, fieldData }) => {
             return headingLength === 1 ? 12 : ((ind + 1) > lastLength ? 6 : 4);
         }
     };
+    const getColSpanMd = (ind, lastLengthMd, headingLengthMd) => {
+        if (ind < lastLengthMd) {
+            return (ind + 1) > lastLengthMd ? 4 : 6;
+        } else {
+            return headingLengthMd === 1 ? 12 : ((ind + 1) > lastLengthMd ? 4 : 6);
+        }
+    };
     return (
         <Grid templateColumns="repeat(12, 1fr)" gap={3}>
             {data?.headings?.length > 0 ? <>
                 {data?.headings?.map((item, ind) => (
                     <>
                         <GridItem colSpan={{
-                            base: 12, md: 6, lg: getColSpanLg(ind, lastLength, headingLength)
+                            base: 12, md: getColSpanMd(ind, lastLengthMd, headingLengthMd), lg: getColSpanLg(ind, lastLength, headingLength)
                         }}>
                             <Card>
                                 <Grid templateColumns="repeat(12, 1fr)" gap={3}>
