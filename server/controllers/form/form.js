e0c3f8eebdf258f107941ba98f8b9e9160eababa
Denish-prolink <prafullsatasia@gmail.com>
2024-03-12 11:18:54 +0530

    Added endpoints to delete a single record and multiple records from a custom module's collection.

--- server/controllers/form/form.js
+++ server/controllers/form/form.js
@@ -31,8 +31,78 @@ const add = async (req, res) => {
         return res.status(200).json({ message: 'Record added successfully', data: newDocument });
 
     } catch (err) {
-        console.error(`Failed to create document`, err);
+        console.error(`Failed to create Record`, err);
         return res.status(400).json({ success: false, message: `Failed to Add Record`, error: err.toString() });
+    }
+};
+
+const deleteField = async (req, res) => {
+    try {
+        if (!req.query?.moduleId) {
+            return res.status(400).send({ success: false, message: "moduleId is required" })
+        }
+
+        const customField = await CustomField.findOne({ _id: req.query?.moduleId }).select("moduleName");
+
+        if (!customField) {
+            return res.status(404).send({ success: false, message: "Module not found" });
+        }
+
+        const collectionExists = await mongoose.connection.db.listCollections({ name: (`${customField?.moduleName}`) }).hasNext();
+
+
+        if (!collectionExists) {
+            return res.status(404).send({ success: false, message: "Collection not exists" })
+        }
+
+        const ExistingModel = mongoose.model(`${customField?.moduleName}`);
+
+        if (typeof ExistingModel !== 'function') {
+            return res.status(500).send({ success: false, message: 'Invalid model' });
+        }
+
+        const result = await ExistingModel.deleteOne({ _id: req.params.id });
+
+        return res.status(200).json({ message: 'Record deleted successfully', data: result });
+
+    } catch (err) {
+        console.error(`Failed to delete Record`, err);
+        return res.status(400).json({ success: false, message: `Failed to delete Record`, error: err.toString() });
+    }
+};
+
+const deleteManyField = async (req, res) => {
+    try {
+        if (!req.body?.moduleId) {
+            return res.status(400).send({ success: false, message: "moduleId is required" })
+        }
+
+        const customField = await CustomField.findOne({ _id: req.body?.moduleId }).select("moduleName");
+
+        if (!customField) {
+            return res.status(404).send({ success: false, message: "Module not found" });
+        }
+
+        const collectionExists = await mongoose.connection.db.listCollections({ name: (`${customField?.moduleName}`) }).hasNext();
+
+
+        if (!collectionExists) {
+            return res.status(404).send({ success: false, message: "Collection not exists" })
+        }
+
+        const ExistingModel = mongoose.model(`${customField?.moduleName}`);
+
+        if (typeof ExistingModel !== 'function') {
+            return res.status(500).send({ success: false, message: 'Invalid model' });
+        }
+
+        const result = await ExistingModel.deleteMany({ _id: { $in: req.body.ids } });
+
+        return res.status(200).json({ message: 'Record deleted successfully', data: result });
+
+    } catch (err) {
+        console.error(`Failed to delete Record`, err);
+        return res.status(400).json({ success: false, message: `Failed to delete Record`, error: err.toString() });
     }
 };
 
@@ -74,9 +144,9 @@ const edit = async (req, res) => {
             return res.status(404).json({ success: false, message: 'Record not found for the given id' });
         }
     } catch (err) {
-        console.error(`Failed to Update document`, err);
+        console.error(`Failed to Update Record`, err);
         return res.status(400).json({ success: false, message: `Failed to Update Record`, error: err.toString() });
     }
 };
 
-module.exports = { add, edit };
+module.exports = { add, edit, deleteField, deleteManyField };
