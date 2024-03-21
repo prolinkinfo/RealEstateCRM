1930f2aab353f056bbed995e1e024aba53614c33
KrushilProlink <krushil.prolink@gmail.com>
2024-03-21 12:46:20 +0530

    fix contact view api issue

--- server/model/schema/contact.js
+++ server/model/schema/contact.js
@@ -27,7 +27,7 @@ const contactSchema = new mongoose.Schema({
     // // 4. Property of Interest
     interestProperty: [{
         type: mongoose.Schema.ObjectId,
-        ref: 'Property',
+        ref: 'Properties',
     }],
     // // 5. History:
     // notesandComments: String,
