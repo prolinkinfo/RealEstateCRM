127d9cd13116efa7c34b0482bba0b2bb7b0c4035
Denish-prolink <prafullsatasia@gmail.com>
2024-03-29 16:00:56 +0530

    "Added contactFields and leadFields imports in config.js; removed leadFields and contactFields arrays from config.js"
    Explanation:
    
    The code diff shows the removal of the `leadFields` and `contactFields` arrays from `config.js` and the addition of imports for `contactFields.js` and `leadFields.js` modules. The arrays have been moved to their respective files and exported for use in `config.js`. This refactoring helps to keep the code organized and maintainable.

---
+++ server/db/leadFields.js
@@ -0,0 +1,84 @@
+const leadFields = [
+    {
+        "name": "leadStatus",
+        "label": "Lead Status",
+        "type": "select",
+        "fixed": false,
+        "delete": false,
+        "belongsTo": null,
+        "backendType": "Mixed",
+        "isTableField": true,
+        "options": [
+            {
+                "name": "Active",
+                "value": "active",
+            },
+            {
+                "name": "Pending",
+                "value": "pending",
+            },
+            {
+                "name": "Sold",
+                "value": "sold",
+            }
+        ],
+        "validation": [
+            {
+                "message": "Invalid type value for Lead Status",
+                "formikType": "String",
+            }
+        ],
+    },
+    {
+        "name": "leadName",
+        "label": "Lead Name",
+        "type": "text",
+        "fixed": true,
+        "delete": false,
+        "belongsTo": null,
+        "backendType": "String",
+        "isTableField": true,
+        "options": [],
+        "validation": [
+            {
+                "require": true,
+                "message": "",
+            },
+        ],
+    },
+    {
+        "name": "leadEmail",
+        "label": "Lead Email",
+        "type": "email",
+        "fixed": true,
+        "delete": false,
+        "belongsTo": null,
+        "backendType": "String",
+        "isTableField": true,
+        "options": [],
+        "validation": [
+            {
+                "require": true,
+                "message": "",
+            },
+        ],
+    },
+    {
+        "name": "leadPhoneNumber",
+        "label": "Lead Phone Number",
+        "type": "tel",
+        "fixed": true,
+        "delete": false,
+        "belongsTo": null,
+        "backendType": "String",
+        "isTableField": true,
+        "options": [],
+        "validation": [
+            {
+                "require": true,
+                "message": "",
+            },
+        ],
+    },
+];
+exports.leadFields = leadFields;
