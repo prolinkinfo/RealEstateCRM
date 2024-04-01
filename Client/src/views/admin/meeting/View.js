88e59e5d0acef2f563e2c630de30d820f5052f8f
Rutubhimani28 <rutubhimani.specscale@gmail.com>
2024-04-01 17:26:18 +0530

    "Add delete meeting functionality and common delete model in Meeting View and Index components"

--- Client/src/views/admin/meeting/View.js
+++ Client/src/views/admin/meeting/View.js
@@ -8,6 +8,9 @@ import { IoIosArrowBack } from "react-icons/io";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { HasAccess } from "../../../redux/accessUtils";
 import { getApi } from "services/api";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { deleteApi } from "services/api";
+import CommonDeleteModel from "components/commonDeleteModel";
 
 const View = () => {
 
@@ -16,11 +19,13 @@ const View = () => {
     const [data, setData] = useState()
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [edit, setEdit] = useState(false);
-    const [deleteModel, setDelete] = useState(false);
+    const [deleteMany, setDeleteMany] = useState(false);
     const user = JSON.parse(localStorage.getItem("user"))
     const size = "lg";
     const [isLoding, setIsLoding] = useState(false)
     const navigate = useNavigate()
+    const params = useParams();
+
 
     const fetchData = async () => {
         setIsLoding(true)
@@ -32,11 +37,26 @@ const View = () => {
         fetchData()
     }, [])
 
-    const [contactAccess, leadAccess] = HasAccess(['Contacts','Leads'])
+    const handleDeleteMeeting = async (ids) => {
+        try {
+            setIsLoding(true)
+            let response = await deleteApi('api/meeting/delete/', params.id)
+            if (response.status === 200) {
+                setDeleteMany(false)
+                navigate(-1)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        finally {
+            setIsLoding(false)
+        }
+    }
+
+    const [permission, contactAccess, leadAccess] = HasAccess(['Meetings', 'Contacts', 'Leads'])
 
     return (
         <>
-
             {isLoding ?
                 <Flex justifyContent={'center'} alignItems={'center'} width="100%" >
                     <Spinner />
@@ -117,10 +137,20 @@ const View = () => {
                         </GridItem>
 
                     </Grid>
-
+                    {(user.role === 'superAdmin' || (permission?.update || permission?.delete)) && <Card mt={3}>
+                        <Grid templateColumns="repeat(6, 1fr)" gap={1}>
+                            <GridItem colStart={6} >
+                                <Flex justifyContent={"right"}>
+                                    {(user.role === 'superAdmin' || permission?.delete) ? <Button size='sm' style={{ background: 'red.800' }} onClick={() => setDeleteMany(true)} leftIcon={<DeleteIcon />} colorScheme="red" >Delete</Button> : ''}
+                                </Flex>
+                            </GridItem>
+                        </Grid>
+                    </Card>
+                    }
 
                 </>}
-
+            {/* Delete model */}
+            <CommonDeleteModel isOpen={deleteMany} onClose={() => setDeleteMany(false)} type='Meetings' handleDeleteData={handleDeleteMeeting} ids={params.id} />
         </>
     );
 };
--- Client/src/views/admin/meeting/index.js
+++ Client/src/views/admin/meeting/index.js
@@ -1,93 +1,3 @@
-// import { AddIcon } from "@chakra-ui/icons";
-// import { Button, Grid, GridItem } from '@chakra-ui/react';
-// import { useEffect, useState } from 'react';
-// import { getApi } from 'services/api';
-// import AddMeeting from "./components/Addmeeting";
-// import CheckTable from './components/CheckTable';
-// import { HasAccess } from "../../../redux/accessUtils";
-
-
-// const Index = () => {
-//     const tableColumns = [
-//         {
-//             Header: "#",
-//             accessor: "_id",
-//             isSortable: false,
-//             width: 10
-//         },
-//         { Header: 'agenda', accessor: 'agenda' },
-//         { Header: "date & Time", accessor: "dateTime", },
-//         { Header: "time stamp", accessor: "timestamp", },
-//         { Header: "create By", accessor: "createdByName", },
-//         { Header: "Action", isSortable: false, center: true },
-
-//     ];
-
-//     const [data, setData] = useState([])
-//     const user = JSON.parse(localStorage.getItem("user"))
-//     const [isLoding, setIsLoding] = useState(false)
-//     const [action, setAction] = useState(false)
-//     const [addMeeting, setMeeting] = useState(false);
-//     const [searchedData, setSearchedData] = useState([]);
-//     const [dynamicColumns, setDynamicColumns] = useState([...tableColumns]);
-//     const [selectedColumns, setSelectedColumns] = useState([...tableColumns]);
-//     const [columns, setColumns] = useState([]);
-//     const [displaySearchData, setDisplaySearchData] = useState(false);
-
-//     const dataColumn = dynamicColumns?.filter(item => selectedColumns?.find(colum => colum?.Header === item.Header))
-
-//     const fetchData = async () => {
-//         setIsLoding(true)
-//         let result = await getApi(user.role === 'superAdmin' ? 'api/meeting' : `api/meeting?createdBy=${user._id}`);
-//         setData(result?.data);
-//         setIsLoding(false)
-//     }
-
-//     const [permission] = HasAccess(['Meeting'])
-
-
-//     // useEffect(() => {
-//     //     fetchData()
-//     // }, [action])
-
-
-//     useEffect(() => {
-//         setColumns(tableColumns)
-//     }, [action])
-
-//     return (
-//         <div>
-
-//             {/* <CheckTable columnsData={columns} tableData={data} /> */}
-//             <CheckTable
-//                 isOpen={addMeeting}
-//                 isLoding={isLoding}
-//                 dataColumn={dataColumn}
-//                 allData={data}
-//                 setSearchedData={setSearchedData}
-//                 setMeeting={setMeeting}
-//                 addMeeting={addMeeting}
-//                 access={permission}
-//                 columnsData={columns}
-//                 from="index"
-//                 setAction={setAction}
-//                 action={action}
-//                 displaySearchData={displaySearchData}
-//                 tableData={displaySearchData ? searchedData : data}
-//                 fetchData={fetchData}
-//                 setDisplaySearchData={setDisplaySearchData}
-//                 setDynamicColumns={setDynamicColumns}
-//                 dynamicColumns={dynamicColumns}
-//                 selectedColumns={selectedColumns}
-//                 setSelectedColumns={setSelectedColumns}
-//                 className='table-fix-container' />
-//             {/* Add Form */}
-//         </div>
-//     )
-// }
-
-// export default Index
-
 import { useEffect, useState } from 'react';
 import { DeleteIcon, EditIcon, ViewIcon } from '@chakra-ui/icons';
 import { Button, Flex, Menu, MenuButton, MenuItem, MenuList, Text, useDisclosure } from '@chakra-ui/react';
@@ -132,10 +42,14 @@ const Index = () => {
                 <Menu isLazy  >
                     <MenuButton><CiMenuKebab /></MenuButton>
                     <MenuList minW={'fit-content'} transform={"translate(1520px, 173px);"}>
-                        {permission?.view && <MenuItem py={2.5} color={'green'} onClick={() => navigate(`/metting/${row?.values._id}`)} icon={<ViewIcon fontSize={15} />}>View</MenuItem>}
+
+                        {permission?.view && <MenuItem py={2.5} color={'green'}
+                            onClick={() => navigate(`/metting/${row?.values._id}`)}
+                            icon={<ViewIcon fontSize={15} />}>View</MenuItem>}
+                        {permission?.delete && <MenuItem py={2.5} color={'red'} onClick={() => { setDeleteMany(true); setSelectedValues([row?.values?._id]); }} icon={<DeleteIcon fontSize={15} />}>Delete</MenuItem>}
                     </MenuList>
                 </Menu>
-            </Text>
+            </Text >
         )
     }
     const tableColumns = [
@@ -160,7 +74,7 @@ const Index = () => {
         setIsLoding(false)
     }
 
-    const handleDeleteMeeting=async(ids)=>{
+    const handleDeleteMeeting = async (ids) => {
         try {
             setIsLoding(true)
             let response = await deleteManyApi('api/meeting/deleteMany', ids)
@@ -233,8 +147,7 @@ const Index = () => {
             <AddMeeting setAction={setAction} isOpen={isOpen} onClose={onClose} />
 
             {/* Delete model */}
-            {/* <Delete isOpen={deleteModel} onClose={setDelete} setSelectedValues={setSelectedValues} url='api/meeting/deleteMany' data={selectedValues} method='many' setAction={setAction} /> */}
-            <CommonDeleteModel isOpen={deleteMany} onClose={()=>setDeleteMany(false)} type='Meetings' handleDeleteData={handleDeleteMeeting} ids={selectedValues}/>
+            <CommonDeleteModel isOpen={deleteMany} onClose={() => setDeleteMany(false)} type='Meetings' handleDeleteData={handleDeleteMeeting} ids={selectedValues} />
         </div>
     )
 }
