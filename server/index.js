9d3dac4129fea37379609a5b2b27e5415ea90a0d
Denish-prolink <denish.prolink@gmail.com>
2025-02-27 10:26:09 +0530

    build folder added, port set using end

--- server/index.js
+++ server/index.js
@@ -1,38 +1,40 @@
-const express = require('express');
-const db = require('./db/config')
-const route = require('./controllers/route');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const port = 5001
-require('dotenv').config()
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const db = require("./db/config");
+const route = require("./controllers/route");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5001;
+const fs = require("fs");
+const path = require("path");
 
 //Setup Express App
 const app = express();
 // Middleware
 app.use(bodyParser.json());
-// Set up CORS  
-app.use(cors())
+// Set up CORS
+app.use(cors());
 //API Routes
-app.use('/api', route);
+app.use("/api", route);
 
-app.get('/', async (req, res) => {
-    res.send('Welcome to my world...')
+app.get("/", async (req, res) => {
+  res.send("Welcome to my world...");
 });
 
 // Get port from environment and store in Express.
 
 const server = app.listen(port, () => {
-    const protocol = (process.env.HTTPS === 'true' || process.env.NODE_ENV === 'production') ? 'https' : 'http';
-    const { address, port } = server.address();
-    const host = address === '::' ? '127.0.0.1' : address;
-    console.log(`Server listening at ${protocol}://${host}:${port}/`);
+  const protocol =
+    process.env.HTTPS === "true" || process.env.NODE_ENV === "production"
+      ? "https"
+      : "http";
+  const { address, port } = server.address();
+  const host = address === "::" ? "127.0.0.1" : address;
+  console.log(`Server listening at ${protocol}://${host}:${port}/`);
 });
 
-
 // Connect to MongoDB
-const DATABASE_URL = process.env.DB_URL || 'mongodb://127.0.0.1:27017'
-const DATABASE = process.env.DB || 'Prolink'
+const DATABASE_URL = process.env.DB_URL || "mongodb://127.0.0.1:27017";
+const DATABASE = process.env.DB || "Prolink";
 
 db(DATABASE_URL, DATABASE);
